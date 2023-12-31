5a85744b69df2e8d99b911ca3c1de6b2d3000e4f
Khoa Cao <146030988+koacow@users.noreply.github.com>
2023-12-30 19:11:12 -0500

    Added UI, per-round info, and resetting feature.

--- script.js
+++ script.js
@@ -8,30 +8,54 @@ function getComputerChoice(){
 }
 
 function playRound (playerSelection, computerSelection){
-    if((playerScore + computerScore) === 5){
-        alert((playerScore >= computerScore) ? 'Player wins!' : 'Computer wins');
-        return;
-    }
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) alert('It\'s a tie!');
+    if (playerSelection === computerSelection) {
+        createRoundResult('It\'s a tie!');
+        currRound++;
+    }
     else if((playerSelection === 'rock' && computerSelection ==='paper')
     || (playerSelection === 'paper' && computerSelection === 'scissors')
     || (playerSelection === 'scissors' && computerSelection === 'rock')){
-        alert('Computer wins! ' + computerSelection + ' beats ' + playerSelection);
         computerScore++;
+        createRoundResult('Computer wins! ' + computerSelection + ' beats ' + playerSelection);
+        currRound++;
     }
     else{
-        alert('You win! ' + playerSelection + ' beats ' + computerSelection);
         playerScore++;
+        createRoundResult('You win! ' + playerSelection + ' beats ' + computerSelection);
+        currRound++;
     }
+    if(playerScore === 3 || computerScore === 3){
+        alert((playerScore >= computerScore) ? 'Player wins!' : 'Computer wins!');
+        resetGame();
+        return;
+    }
+}
+
+function createRoundResult(message){
+    const newResult = document.createElement('li');
+    newResult.textContent = 'Round ' + currRound + ': ' + message + '. Player\'s score: ' + playerScore + '/Computer\'s score: ' + computerScore;
+    results.appendChild(newResult);
+}
+
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    currRound = 1;
+    results.replaceChildren('Results: ');
 }
 let playerScore = 0;
 let computerScore = 0;
+let currRound = 1;
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const restart = document.querySelector('#restart');
+const results = document.querySelector('li');
 
 rock.addEventListener('click', () => playRound('rock',getComputerChoice()));
 paper.addEventListener('click', () => playRound('paper',getComputerChoice()));
 scissors.addEventListener('click', () => playRound('scissors',getComputerChoice())); 
+
+restart.addEventListener('click', () => resetGame());
